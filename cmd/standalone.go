4ae9497f56663e519fff08a7ca663fdd0df0f385
Conrad Constantine <conrad@1211.net>
2020-06-01 11:24:42 -0700

    standalone mode entrycmd

--- cmd/standalone.go
+++ cmd/standalone.go
@@ -1,13 +1,63 @@
-// Standalone Mode - collector and processor in single instance.package, either from local interface or PCap data
+/*
+Copyright © 2020 NAME HERE <EMAIL ADDRESS>
 
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package cmd
 
-import "github.com/databeast/goatherd/collectors"
+import (
+	"fmt"
 
-func StandaloneMode() {
-	collector, err := collectors.NewPcapFileCollector()
-	if err != nil {
-		println(err.Error())
-	}
-	collector.Load("sample.pcap")
+	"github.com/databeast/goatherd/collectors"
+	"github.com/spf13/cobra"
+)
+
+// standaloneCmd represents the standalone command
+var standaloneCmd = &cobra.Command{
+	Use:   "standalone",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("standalone called")
+		collector, err := collectors.NewPcapFileCollector()
+		if err != nil {
+			println(err.Error())
+			return
+		}
+		err = collector.Load("sample.pcap")
+		if err != nil {
+			println(err.Error())
+			return
+		}
+
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(standaloneCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// standaloneCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// standaloneCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
