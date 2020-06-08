9aee9aefcbc0f95140f9e21231789214e4a8708e
Conrad Constantine <conrad@1211.net>
2020-06-08 12:35:16 -0700

    base server and pb gencode

--- mapper/ingest/ingester.go
+++ mapper/ingest/ingester.go
@@ -1,1 +1,31 @@
 package ingest
+
+import (
+	"context"
+	"github.com/databeast/goatherd/packets"
+	"google.golang.org/grpc"
+)
+
+type ingestsrv struct {
+	grpcsrv *grpc.Server
+}
+
+func (i *ingestsrv) Ingest(server packets.PacketCollection_IngestServer) error {
+	panic("implement me")
+}
+
+func (i *ingestsrv) CapturePoint(ctx context.Context, point *packets.RegisterCapturePoint) (*packets.RegisterResponse, error) {
+	panic("implement me")
+}
+
+var ingester *ingestsrv
+
+
+func BeginRemoteIngest() {
+	ingester = &ingestsrv{
+		grpcsrv:  grpc.NewServer(),
+	}
+
+	packets.RegisterPacketCollectionServer(ingester.grpcsrv, ingester)
+
+}
